33eaf1401174c9ee2361ed7453a39a512ebec282
zwgzlh <43113531+zwgzlh@users.noreply.github.com>
2018-09-17 01:05:49 +0700

    Add files via upload

---
+++ lesson 4.js
@@ -0,0 +1,17 @@
+﻿//задание 1
+let max = 999;
+    let digit = {
+         number: Math.round (Math.random() * max),
+         units: 0,
+         tens: 0,
+         hundreds: 0,
+};
+        if (digit.number <= 9){
+          digit.units = digit.number;
+        } else if (digit.number <= 999){
+          digit.units =  digit.number / 10;
+          digit.tens = digit.number / 10 % 10;
+          digit.hundreds = digit.number / 100 % 10 ;
+        }
+        console.log(digit);
+
---
+++ lesson-3.js
@@ -0,0 +1,43 @@
+﻿
+//задание 1
+nextPrime:
+for (var i = 2; i <= 100; i++) {
+
+for (var j = 2; j < i; j++) {
+if (i % j == 0) continue nextPrime;
+    }
+
+alert (i);
+
+   }
+// задание 2
+var basket = [
+    ["Оправа", 12000, 1],
+    ["Линзы", 5000, 2]
+ ];
+
+function countBasketPrice (basket){
+	var sum = 0;
+
+	for (var i = 0; i< basket.length; i++) {
+		sum += basket[i][1] * basket[i][2]
+	}
+
+	return sum;
+}
+console.log (countBasketPrice(basket));
+
+
+
+//задание 3
+var i;
+for ( i = 0; i <= 9; i++){
+console.log(i);
+    }
+
+//задание 4
+for ( var str = 'x', i = 0; i < 20;  i++) {
+	console.log (str);
+	str += 'x';
+
+}
