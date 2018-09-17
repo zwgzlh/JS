078692661929859c1147c644a12eb1b8895004e4
zwgzlh <43113531+zwgzlh@users.noreply.github.com>
2018-09-17 15:33:50 +0700

    Update lesson 4.js

--- lesson 4.js
+++ lesson 4.js
@@ -1,20 +1,20 @@
 ﻿//задание 1
-let max = 999;
-    let digit = {
-         number: Math.round (Math.random() * max),
-         units: 0,
-         tens: 0,
-         hundreds: 0,
-};
-        if (digit.number <= 9){
-          digit.units = digit.number;
-        } else if (digit.number <= 999){
-          digit.units =  digit.number / 10;
-          digit.tens = digit.number / 10 % 10;
-          digit.hundreds = digit.number / 100 % 10 ;
-        }
-        console.log(digit);
+function random(max) {
+  return Math.random() * max;
+}
 
+function countDigits(value) {
+  if (value > 999) {
+    return {};
+  }
+
+  let digitInfo = { number: value, hundreds: 0, tens: 0, units: 0 };
+  digitInfo.hundreds = Math.trunc(value / 100);
+  digitInfo.tens = Math.trunc((value % 100) / 10);
+  digitInfo.units = Math.trunc((value % 100) % 10);
+
+  return digitInfo;
+}
 ﻿//задание 2
 let glasses  = {
     name: “Оправа”,
