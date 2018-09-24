831b15279403659e70c6706d7fe389f4be0c11e1
zwgzlh <esmolentseva@gmail.com>
2018-09-24 17:56:23 +0700

    lesson6 lesson6

---
+++ main.js
@@ -0,0 +1,27 @@
+﻿var basket = []
+
+function registerPurchaseListeners() {
+		let items = document.getElementById('store').children
+		for (let i = 0; i < items.length; i++) {
+			let item = items[i]			
+			item.addEventListener('click', handlePurchase.bind(this, item))
+		}		
+}
+
+function handlePurchase(item, event) {
+	let product = { title: item.children[0].innerText,
+					price: Number.parseFloat(item.children[1].innerText.match(/\d+/)[0]) }
+	basket.push(product)
+	calculatePrice()
+}
+
+function calculatePrice() {
+	let sum = 0;
+	basket.forEach(function (item, basket) {
+		sum += item.price
+	})
+
+	document.getElementById('total-price').innerText = 'Сумма: ' + sum
+}
+
+window.onload = registerPurchaseListeners
